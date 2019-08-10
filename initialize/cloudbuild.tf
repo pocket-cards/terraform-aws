2c81f24315bb94a188ba866641ee215035f80569
wwalpha <wwalpha@gmail.com>
2019-08-10 12:02:14 +0900

    update timeout

--- initialize/cloudbuild.tf
+++ initialize/cloudbuild.tf
@@ -5,7 +5,7 @@ resource "aws_codebuild_project" "initialize" {
   depends_on = ["aws_iam_role.codebuild_role"]
 
   name          = "${local.project_name_uc}-Initialize"
-  build_timeout = "5"
+  build_timeout = "30"
   service_role  = "${aws_iam_role.codebuild_role.arn}"
 
   artifacts {
@@ -66,3 +66,11 @@ resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   role       = "${aws_iam_role.codebuild_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+# resource "aws_iam_role_policy" "codebuild_role_policy" {
+#   name   = "AcrossBucketPolicy"
+#   role   = "${aws_iam_role.codebuild_role.id}"
+#   policy = "${file("iam/codebuild_policy_across_bucket.json")}"
+
+#   count = "${local.is_dev ? 0 : 1}"
+# }
