4bb6ce2a02eb61b68b3002fdf86d720573ec9748
wwalpha <wwalpha@gmail.com>
2019-08-10 11:18:35 +0900

    update

--- initialize/locals.tf
+++ initialize/locals.tf
@@ -8,4 +8,11 @@ locals {
   bucket_name_artifacts = "${local.project_name}-artifacts-${random_id.bucket.hex}"
   domain_prefix         = "${local.is_dev ? "dev." : ""}"
   # dns_name_servers = "${length(var.dns_name_servers) != 0 ? var.dns_name_servers : local.name_servers}"
+
+  # -----------------------------------------------
+  # CodeBuild
+  # -----------------------------------------------
+  build_type         = "LINUX_CONTAINER"
+  build_compute_type = "BUILD_GENERAL1_SMALL"
+  build_image        = "aws/codebuild/standard:2.0"
 }
