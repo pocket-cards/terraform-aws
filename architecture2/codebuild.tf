3b2fb4815b6ab7c1eac0354cd8d0d603b14c1dc9
wwalpha <wwalpha@gmail.com>
2019-08-15 23:20:41 +0900

    add build envs

--- architecture2/codebuild.tf
+++ architecture2/codebuild.tf
@@ -37,6 +37,18 @@ resource "aws_codebuild_project" "codebuild_frontend" {
       name  = "API_URL"
       value = "https://${local.api_domain_name}"
     }
+    environment_variable {
+      name  = "IDENTITY_POOL_ID"
+      value = "${aws_cognito_identity_pool.this.id}"
+    }
+    environment_variable {
+      name  = "USER_POOL_ID"
+      value = "${aws_cognito_user_pool.this.id}"
+    }
+    environment_variable {
+      name  = "USER_POOL_WEB_CLIENT_ID"
+      value = "${aws_cognito_user_pool_client.this.id}"
+    }
   }
 
   source {
--- architecture2/outputs.tf
+++ architecture2/outputs.tf
@@ -1,24 +1,15 @@
 # output "cloudfront_ditribution_id" {
 #   value = "${aws_cloudfront_distribution.this.id}"
 # }
-output "user_pool_id" {
-  value = "${aws_cognito_user_pool.this.id}"
-}
-# output "user_pool_arn" {
-#   value = "${aws_cognito_user_pool.this.arn}"
-# }
-# output "user_pool_endpoint" {
-#   value = "${aws_cognito_user_pool.this.endpoint}"
-# }
-output "user_pool_web_client_id" {
-  value = "${aws_cognito_user_pool_client.this.id}"
-}
 output "identity_pool_id" {
   value = "${aws_cognito_identity_pool.this.id}"
 }
-# output "identity_pool_arn" {
-#   value = "${aws_cognito_identity_pool.this.arn}"
-# }
+output "user_pool_id" {
+  value = "${aws_cognito_user_pool.this.id}"
+}
+output "user_pool_web_client_id" {
+  value = "${aws_cognito_user_pool_client.this.id}"
+}
 output "web_domain_name" {
   value = "${aws_acm_certificate.web.domain_name}"
 }
